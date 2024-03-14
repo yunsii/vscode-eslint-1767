60cfc0fa117fb52829c36a3c5710b36ac875937e
Yuns <yuns.xie@qq.com>
2024-03-14 21:58:15 +0800

    feat: use @antfu/eslint-config

--- eslint.config.js
+++ eslint.config.js
@@ -1,33 +1,3 @@
-// migration-guide: https://eslint.org/docs/latest/use/configure/migration-guide
+import antfu from '@antfu/eslint-config'
 
-import js from "@eslint/js";
-import unusedImports from 'eslint-plugin-unused-imports'
-import { parser } from "typescript-eslint";
-import stylisticTs from '@stylistic/eslint-plugin-ts'
-
-export default [ 
-  js.configs.recommended,
-  {
-    files: ["**/*.js"],
-    plugins: {
-      'unused-imports': unusedImports
-    },
-    rules: {
-      "unused-imports/no-unused-imports": "error",
-    }
-  },
-  {
-    files: ["**/*.ts"],
-    languageOptions: {
-      parser: parser
-    },
-    plugins: {
-      '@stylistic/ts': stylisticTs,
-      'unused-imports': unusedImports,
-    },
-    rules: {
-      '@stylistic/ts/indent': ['error', 2],
-      "unused-imports/no-unused-imports": "error",
-    }
-  }
-]
+export default antfu()
