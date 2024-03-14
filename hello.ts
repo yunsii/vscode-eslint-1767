5d8e1822c61fc4011e336a278a9f2b438eebdbb9
Yuns <yuns.xie@qq.com>
2024-03-14 21:34:29 +0800

    chore: init project

---
+++ hello.ts
@@ -0,0 +1,3 @@
+import { chalk, fs } from 'zx'
+
+const result = fs.readJsonSync('./package.json')
---
+++ index.js
@@ -0,0 +1,4 @@
+import { chalk, fs } from 'zx'
+
+const result = fs.readJsonSync('./package.json')
+console.log('🚀 ~ file: index.js:4 ~ result:', result)
